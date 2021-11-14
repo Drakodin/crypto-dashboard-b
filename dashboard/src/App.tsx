d410474d975aa394c15fc3c7aa3d14acd814dcb8
heatherzhu <heatherzhu17@outlook.com>
2021-11-14 06:44:55 -0500

    Removed unused imports and class names/ids and added some styling

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from './NavBar';
+import Dashboard from './Dashboard';
+import Holdings from './Holdings';
+import Browse from './Browse';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Router>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path='dashboard/*' element={<Dashboard />} />
+          <Route path='holdings/*' element={<Holdings />} />
+          <Route path='browse/*' element={<Browse />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- dashboard/src/NavBar.tsx
+++ dashboard/src/NavBar.tsx
@@ -1,24 +1,22 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
-import Dashboard from './Dashboard';
-import Holdings from './Holdings';
-import Browse from './Browse';
 
 class NavBar extends React.Component {
     render() {
         return (
-            <Navbar fixed='top' bg='dark' variant='dark'>
-                <Container>
-                    <Navbar.Collapse id='basic-navbar-nav'>
-                        <Nav className='me-auto'>
-                            <Nav.Link href='/dashboard'>Dashboard</Nav.Link>
-                            <Nav.Link href='/holdings'>Your Holdings</Nav.Link>
-                            <Nav.Link href='/browse'>Browse Assets</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <div id='nav'>
+                <Navbar>
+                    <Container>
+                        <Navbar.Collapse>
+                            <Nav>
+                                <Nav.Link className='link' href='/dashboard'>Dashboard</Nav.Link>
+                                <Nav.Link className='link' href='/holdings'>Your Holdings</Nav.Link>
+                                <Nav.Link className='link' href='/browse'>Browse Assets</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
         );
     }
 }
